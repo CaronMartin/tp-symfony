abd1fb462ab2ea116af650c2121972ccba6e3164
CaronMartin <martin.caron@ig2i.centralelille.fr>
2021-03-26 14:02:18 +0100

    Fin du TP Twig

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -8,27 +8,23 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class LuckyController
 {
-    /**
-    * @Route("/lucky_number", name="app_lucky_number")
-    */
+     /**
+      * @Route("/lucky_number", name="app_lucky_number")
+      */
     public function number(Request $request): Response
     {
-        $number = random_int(0, 100);
         $user = [
             'name' => 'John Doe',
             'place' => 'Lens',
         ];
 
         dump($user);
+        $number = random_int(0, 100);
         $name = $request->query->get('name');
 
         return new Response(
-            "<html>
-                        <body>
-                            <p>Lucky number: $number</p>
-                            <p>Name : $name</p>
-                        </body>
-                    </html>"
+            "<html><body><p>User: $name ,Lucky number: $number</p></body></html>"
         );
     }
 }
+?>
